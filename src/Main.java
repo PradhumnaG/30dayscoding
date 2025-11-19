3f7be9d2e57fafaaf6a244cecaf4cd9d8f4f49ec
PradhumnaG <142740452+PradhumnaG@users.noreply.github.com>
2025-11-20 00:05:37 +0530

    first commit

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,5 @@ public class Main {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 2, 4, 5, 3};
         System.out.println("Contains duplicate: " +Duplicate1(nums));
-        }
     }
-q
+    }
